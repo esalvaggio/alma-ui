c00c16585fcdd3a2762d59e8aad64d1bc556b241
esalvaggio <elliotms98@gmail.com>
2024-02-04 01:07:34 -0800

    cleanup

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar';
-// Import any other layout-specific styles if needed
+
 const MainLayout = () => {
     return (
         <div className="mainLayout">
