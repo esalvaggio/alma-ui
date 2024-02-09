2fb22bdd3a204daf550944ef969d0e8dc69b5158
esalvaggio <elliotms98@gmail.com>
2024-02-08 23:27:42 -0800

    Essay page css progress

--- src/pages/EssayPage/index.tsx
+++ src/pages/EssayPage/index.tsx
@@ -3,11 +3,16 @@ const EssayPage = () => {
     return (
         <div className={style.essayPage}>
             <div className={style.pageContainer}>
-                <div className={style.header}>
-                    <div className={style.closeIcon}>X</div>
-                </div>
-                <div className={style.essayContainer}>
-                    essay
+                <div className={style.essayPageContainer}>
+                    <div className={style.header}>
+                        <button className={style.closeIcon}>X</button>
+                    </div>
+                    <div className={style.essayContainer}>
+                        <div className={style.essayContent}>
+
+                        </div>
+                    </div>
+                    <div className={style.footer}></div>
                 </div>
             </div>
         </div>
