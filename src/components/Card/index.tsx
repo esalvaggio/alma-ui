689efe5958dd7f8985954e36041ff4e62cb0d3d4
Elliot Salvaggio <elliotms98@gmail.com>
2024-05-24 15:19:04 -0500

    Card CSS

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { CardData } from '../../interfaces'
 import style from './index.module.scss'
 
@@ -6,11 +7,24 @@ interface CardComponent {
     data: CardData
 }
 const Card = ({ onAnswer, data }: CardComponent) => {
+    const [answered, setAnswered] = useState(false)
     return (
-        <div className={style.card}>
+        <div className={`${style.card} ${!answered && style.cardUnanswered}`} onClick={() => setAnswered(true)}>
             <div className={style.cardContent}>
                 <h1 className={style.questionText}>{data.question}</h1>
-                <div className={style.continueText}>click anywhere to continue</div>
+                <div className={style.contentWrapper}>
+                    {!answered ? (
+                        <div className={style.continueText}>click anywhere to continue</div>
+                    ) : (
+                        <div className={style.answerContainer}>
+                            <div className={style.answer}>{data.answer}</div>
+                            <div className={style.buttonContent}>
+                                <button className={style.answerButton} onClick={() => console.log("remembered")}>Remembered</button>
+                                <button className={style.answerButton} onClick={() => console.log("didnt remembered")}>Didn't Remember</button>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
