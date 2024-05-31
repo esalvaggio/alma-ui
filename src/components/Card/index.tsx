e2e87af8f33c2eebf7440e1328f2b814cfcc8548
Elliot Salvaggio <elliotms98@gmail.com>
2024-05-30 22:38:15 -0700

    Card footer + fonts

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,21 +9,32 @@ interface CardComponent {
 const Card = ({ onAnswer, data }: CardComponent) => {
     const [answered, setAnswered] = useState(false)
     return (
-        <div className={`${style.card} ${!answered && style.cardUnanswered}`} onClick={() => setAnswered(true)}>
-            <div className={style.cardContent}>
-                <h1 className={style.questionText}>{data.question}</h1>
-                <div className={style.contentWrapper}>
-                    {!answered ? (
-                        <div className={style.continueText}>click anywhere to continue</div>
-                    ) : (
-                        <div className={style.answerContainer}>
-                            <div className={style.answer}>{data.answer}</div>
-                            <div className={style.buttonContent}>
-                                <button className={style.answerButton} onClick={() => console.log("remembered")}>Remembered</button>
-                                <button className={style.answerButton} onClick={() => console.log("didnt remembered")}>Didn't Remember</button>
+        <div className={style.cardGroup}>
+            <div className={`${style.card} ${style.mainCard} ${!answered && style.cardUnanswered}`} onClick={() => setAnswered(true)}>
+                <div className={style.cardContent}>
+                    <h1 className={style.questionText}>{data.question}</h1>
+                    <div className={style.contentWrapper}>
+                        {!answered ? (
+                            <div className={style.continueText}>click anywhere to continue</div>
+                        ) : (
+                            <div className={style.answerContainer}>
+                                <div className={style.answer}>{data.answer}</div>
+                                <div className={style.buttonContent}>
+                                    <button className={style.answerButton} onClick={() => console.log("remembered")}>Remembered</button>
+                                    <button className={style.answerButton} onClick={() => console.log("didnt remembered")}>Didn't Remember</button>
+                                </div>
                             </div>
-                        </div>
-                    )}
+                        )}
+                    </div>
+                </div>
+            </div>
+            <div className={`${style.card} ${style.cardFooter}`}>
+                <div className={style.footerContent}>
+                    <div className={style.footerInfo}>
+                        <div className={style.footerTitle}>It's fair to describe Schitzofrenia as probably mostly genetic</div>
+                        <div className={style.footerDate}>Reading Today, May 24, 2024</div>
+                    </div>
+                    <div className={style.footerTimeline}>In-Text – 5 Days - 2 Weeks – 1 Month – 2 Months – *</div>
                 </div>
             </div>
         </div>
