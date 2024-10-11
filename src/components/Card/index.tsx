e976328fb96e7dabbad2de76800e62d9b0113ea6
Elliot Salvaggio <elliotms98@gmail.com>
2024-10-11 12:16:31 -0700

    temporary hide cards after selecting

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,8 +9,10 @@ interface CardComponent {
 }
 const Card = ({ cardData }: CardComponent) => {
     const [answered, setAnswered] = useState(false)
+    const [showCard, setShowCard] = useState(true)
 
     const handleCardAnswer = (correct: boolean) => {
+        setShowCard(false)
         answerCard(correct)
     }
     const answerCard = async (answer: boolean) => {
@@ -30,33 +32,35 @@ const Card = ({ cardData }: CardComponent) => {
     }
     return (
         <div className={style.cardGroup}>
-            <div className={`${style.card} ${style.mainCard} ${!answered && style.cardUnanswered}`} onClick={() => setAnswered(true)}>
-                <div className={style.cardContent}>
-                    <h1 className={style.questionText}>{cardData.question}</h1>
-                    <div className={style.contentWrapper}>
-                        {!answered ? (
-                            <div className={style.continueText}>click anywhere to continue</div>
-                        ) : (
-                            <div className={style.answerContainer}>
-                                <div className={style.answer}>{cardData.answer}</div>
-                                <div className={style.buttonContent}>
-                                    <button className={style.answerButton} onClick={() => handleCardAnswer(true)}>Remembered</button>
-                                    <button className={style.answerButton} onClick={() => handleCardAnswer(false)}>Didn't Remember</button>
-                                </div>
+            {showCard ?
+                <>
+                    <div className={`${style.card} ${style.mainCard} ${!answered && style.cardUnanswered}`} onClick={() => setAnswered(true)}>
+                        <div className={style.cardContent}>
+                            <h1 className={style.questionText}>{cardData.question}</h1>
+                            <div className={style.contentWrapper}>
+                                {!answered ? (
+                                    <div className={style.continueText}>click anywhere to continue</div>
+                                ) : (
+                                    <div className={style.answerContainer}>
+                                        <div className={style.answer}>{cardData.answer}</div>
+                                        <div className={style.buttonContent}>
+                                            <button className={style.answerButton} onClick={() => handleCardAnswer(true)}>Remembered</button>
+                                            <button className={style.answerButton} onClick={() => handleCardAnswer(false)}>Didn't Remember</button>
+                                        </div>
+                                    </div>
+                                )}
                             </div>
-                        )}
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className={`${style.card} ${style.cardFooter}`}>
-                <div className={style.footerContent}>
-                    <div className={style.footerInfo}>
-                        <div className={style.footerTitle}>Essay Title Goes Here</div>
-                        <div className={style.footerDate}>Reading Today, May 24, 2024</div>
-                    </div>
-                    <div className={style.footerTimeline}>In-Text – 5 Days - 2 Weeks – 1 Month – 2 Months – *</div>
-                </div>
-            </div>
+                    <div className={`${style.card} ${style.cardFooter}`}>
+                        <div className={style.footerContent}>
+                            <div className={style.footerInfo}>
+                                <div className={style.footerTitle}>Essay Title Goes Here</div>
+                                <div className={style.footerDate}>Reading Today, May 24, 2024</div>
+                            </div>
+                            <div className={style.footerTimeline}>In-Text – 5 Days - 2 Weeks – 1 Month – 2 Months – *</div>
+                        </div>
+                    </div> </> : <></>}
         </div>
     )
 }
